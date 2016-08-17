629ae8dd541eeff913fd8a310ee5ba24f9fc7022
Roland Meertens <rolandmeertens@gmail.com>
2016-08-17 17:39:32 +0200

    demo day working

--- sw/airborne/modules/location_sender/location_sender.c
+++ sw/airborne/modules/location_sender/location_sender.c
@@ -40,17 +40,13 @@ int flyIndexOtherDrone=0;
 
 void addPosition(struct LlaCoor_i new_pos){
 	if(writeIndexOtherDrone<MAX_LENGTH_WAYPOINTER){
-		  printf("adding %d %d %d\n",new_pos.lat,new_pos.lon,new_pos.alt);
 		historyOtherDrone[writeIndexOtherDrone++]=new_pos;
 	}
 }
 uint8_t setWpToNextNUSDemo(uint8_t wp_id){
-	printf("Set wp to next! %d ",flyIndexOtherDrone );
 	if(flyIndexOtherDrone < writeIndexOtherDrone){
 	  struct LlaCoor_i c = historyOtherDrone[flyIndexOtherDrone];
 	  struct LlaCoor_i *myOwnPos = stateGetPositionLla_i();
-	  printf("Going to  %d %d %d\n",c.lat,c.lon,c.alt);
-	  printf("My own %d %d %d\n",myOwnPos->lat,myOwnPos->lon,myOwnPos->alt);
 //	  waypoint_set_latlon(wp_id,&c);
 	  waypoint_set_lla(wp_id,&c);
 	  flyIndexOtherDrone++;
@@ -58,13 +54,11 @@ uint8_t setWpToNextNUSDemo(uint8_t wp_id){
 	return 0;
 }
 void send_current_location() {
-	printf("Sending lcoation\n");
 	if(stateGetPositionEnu_f()->z > 0.25){
 	  struct LlaCoor_i *c = stateGetPositionLla_i();
 	  int32_t la = c->lat;
 	  int32_t lo = c->lon;
 	  int32_t al = c->alt;
-
 	  DOWNLINK_SEND_ROLANDLOC(DefaultChannel, DefaultDevice, &la,&lo,&al);
 	}
 }
@@ -78,26 +72,14 @@ uint8_t reset_write(){
 	return false;
 }
 void parse_this(){
-	printf("Parsing this");
-	struct LlaCoor_i new_wp;
-	  uint8_t ac_id = DL_MOVE_WP_ac_id(dl_buffer);
+  struct LlaCoor_i new_wp;
+  uint8_t ac_id = DL_MOVE_WP_ac_id(dl_buffer);
+  uint8_t wp_id = DL_MOVE_WP_wp_id(dl_buffer);
+  new_wp.lat = DL_MOVE_WP_lat(dl_buffer);
+  new_wp.lon = DL_MOVE_WP_lon(dl_buffer);
+  new_wp.alt = DL_MOVE_WP_alt(dl_buffer);
+  addPosition(new_wp);
 
-	  uint8_t wp_id = DL_MOVE_WP_wp_id(dl_buffer);
-
-	  new_wp.lat = DL_MOVE_WP_lat(dl_buffer);
-
-	  new_wp.lon = DL_MOVE_WP_lon(dl_buffer);
-
-	  new_wp.alt = DL_MOVE_WP_alt(dl_buffer);
-	  addPosition(new_wp);
-
-/*	  uint8_t leader = DL_FORMATION_STATUS_leader_id(dl_buffer);
-	  uint8_t status = DL_FORMATION_STATUS_status(dl_buffer);
-	  if (ac_id == AC_ID) { leader_id = leader; }
-	  else if (leader == leader_id) { updateFormationStatus(ac_id, status); }
-	  else { updateFormationStatus(ac_id, UNSET); }
-	}
-*/
 }
 
 
--- sw/ground_segment/python/playthrough.py
+++ sw/ground_segment/python/playthrough.py
@@ -23,29 +23,17 @@ from pprzlink.message import PprzMessage
 aircrafts = {}
 interface = IvyMessagesInterface("Paparazzi Messages Viewer")
 detect_ac_id=15
-avoid_ac_id=16
+avoid_ac_id=[16,17]
 def message_recv(ac_id, msg):
 	if ac_id==detect_ac_id and msg.name=='ROLANDLOC': 
-	  	#print("Received msg from ", ac_id, " messag: ", msg)
-		#print 	dir(msg)
-		#print msg.name
-		#print msg.msg_id
-
-		#msg = PprzMessage("datalink", "ROLAND_GO_HERE")
-		smsg = PprzMessage("datalink", "MOVE_WP")
-		smsg['wp_id'] = 1
-		smsg['ac_id'] = avoid_ac_id
-		smsg['lat'] = msg['lat']
-		smsg['lon'] = msg['lon']
-		smsg['alt'] = msg['alt']
-		
-		#print("Sending message: %s" % smsg)
-		interface.send(smsg)
-#  if ac_id in self.aircrafts and msg.name in self.aircrafts[ac_id].messages:
-#    if time.time() - self.aircrafts[ac_id].messages[msg.name].last_seen < 0.2:
-#	return
-
-	
+		for rec_ac in avoid_ac_id:
+			smsg = PprzMessage("datalink", "MOVE_WP")
+			smsg['wp_id'] = 1
+			smsg['ac_id'] = rec_ac
+			smsg['lat'] = msg['lat']
+			smsg['lon'] = msg['lon']
+			smsg['alt'] = msg['alt']
+			interface.send(smsg)
 interface.subscribe(message_recv)
 
 
